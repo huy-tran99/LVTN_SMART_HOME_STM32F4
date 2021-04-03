38d7802d0e189a66d2bdfc9d20b4495d6f900c09
nguyenvanquyet1498 <1612874@hcmut.edu.vn>
2021-04-03 15:08:35 +0700

    keypad

---
+++ Software/KeyPad_LCD/MDK-ARM/KeyPad_LCD/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+keypad_lcd\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ Software/KeyPad_LCD/lib_keypad/lib_keypad.h
@@ -0,0 +1,7 @@
+#include "stm32f4xx_hal.h"
+#include "i2c-lcd.h"
+
+char read_keypad (void);
+void dangnhap(void);
+	void doimatkhau(void);
+
